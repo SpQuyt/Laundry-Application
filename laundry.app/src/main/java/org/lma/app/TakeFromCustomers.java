8a979963cc71df5c9b173db456f306e61e756dcd
Trương Anh Quốc <quocta@talent.ominext.com>
2019-03-13 01:16:42 +0700

    complete basic GUI of printForm

--- laundry.app/src/main/java/org/lma/app/TakeFromCustomers.java
+++ laundry.app/src/main/java/org/lma/app/TakeFromCustomers.java
@@ -1,20 +1,8 @@
 package org.lma.app;
 
-import java.awt.Font;
-
-import javax.swing.ButtonGroup;
-import javax.swing.ImageIcon;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JTabbedPane;
-import javax.swing.JTextField;
-import javax.swing.UIManager;
-import javax.swing.UnsupportedLookAndFeelException;
-import javax.swing.JCheckBox;
-import javax.swing.JRadioButton;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 
 @SuppressWarnings("serial")
 public class TakeFromCustomers extends JFrame {
@@ -27,7 +15,32 @@ public class TakeFromCustomers extends JFrame {
 	private JPanel thanhToan;
 	public ImageIcon img = new ImageIcon("./icon app.png");
 	private JTextField nameField;
-	private JTextField weightField;
+	private JTextField weightField1;
+	private JTextField weightField2;
+	private JCheckBox khoAoKhoac;
+	private JTextField to1Field;
+	private JTextField vua1Field;
+	private JTextField nho1Field;
+	private JCheckBox chan;
+	private JTextField to2Field;
+	private JTextField vua2Field;
+	private JTextField nho2Field;
+	private JLabel to1;
+	private JLabel vua1;
+	private JLabel nho1;
+	private JLabel to2;
+	private JLabel vua2;
+	private JLabel nho2;
+	private JLabel tenDV;
+	private JTextField textField;
+	private JLabel soTien;
+	private JTextField textField_1;
+	private JRadioButton traTruoc;
+	private JRadioButton traSau;
+	private JLabel totalAmount;
+	private JLabel numberTotal;
+	private int numberTotalAmount = 0;
+	private JButton printButton;
 
 	public static void main(String[] args) {
 		new TakeFromCustomers();
@@ -41,6 +54,16 @@ public class TakeFromCustomers extends JFrame {
 		this.frame.setContentPane(this.contentPanel);
 		this.frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		this.frame.getContentPane().setLayout(null);
+		
+		JLabel tenKH = new JLabel("Tên Khách Hàng");
+		tenKH.setBounds(49, 48, 155, 42);
+		contentPanel.add(tenKH);
+		tenKH.setFont(new Font("Times New Roman", Font.PLAIN, 20));
+		
+		nameField = new JTextField();
+		nameField.setBounds(214, 54, 335, 37);
+		contentPanel.add(nameField);
+		nameField.setColumns(10);
 		
 		this.frame.setBounds(350, 200, 675, 496);
 		this.frame.setVisible(true);
@@ -66,83 +89,346 @@ public class TakeFromCustomers extends JFrame {
 	}
 	
 	public void createDichVuChinh(JTabbedPane tabbedPane) {
-		ButtonGroup group = new ButtonGroup();
-		
+		ButtonGroup group1 = new ButtonGroup();
+		ButtonGroup group2 = new ButtonGroup();
+
 		dvChinh = new JPanel();
 		dvChinh.setLayout(null);
 		tabbedPane.addTab("DỊCH VỤ GIẶT CHÍNH", null, dvChinh, null);
 		
-		JLabel tenKH = new JLabel("Tên Khách Hàng");
-		tenKH.setBounds(32, 11, 166, 65);
-		dvChinh.add(tenKH);
-		tenKH.setFont(new Font("Times New Roman", Font.PLAIN, 20));
+		///////////////////////////////////////////////////////////////////////GROUP BUTTON 1
+		final JCheckBox check1 = new JCheckBox(" Khối lượng giặt sấy");
+		check1.setFont(new Font("Times New Roman", Font.PLAIN, 22));
+		check1.setBounds(17, 51, 213, 35);
+		dvChinh.add(check1);
 		
-		JLabel khoiLuongGiat = new JLabel("Khối lượng giặt");
-		khoiLuongGiat.setFont(new Font("Times New Roman", Font.PLAIN, 20));
-		khoiLuongGiat.setBounds(32, 112, 166, 65);
-		dvChinh.add(khoiLuongGiat);
+		weightField1 = new JTextField();
+		weightField1.setBounds(320, 37, 126, 20);
+		dvChinh.add(weightField1);
+		weightField1.setColumns(10);
 		
-		nameField = new JTextField();
-		nameField.setBounds(196, 21, 216, 51);
-		dvChinh.add(nameField);
-		nameField.setColumns(10);
-		
-		weightField = new JTextField();
-		weightField.setBounds(286, 137, 126, 20);
-		dvChinh.add(weightField);
-		weightField.setColumns(10);
-		
-		JRadioButton weightAfter = new JRadioButton("Cân sau");
-		weightAfter.addActionListener(new ActionListener() {
+		final JRadioButton weightAfter1 = new JRadioButton("Cân sau");
+		weightAfter1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				weightField.setEnabled(false);
+				weightField1.setEnabled(false);
 			}
 		});
-		weightAfter.setBounds(196, 177, 84, 23);
-		dvChinh.add(weightAfter);
+		weightAfter1.setBounds(230, 77, 84, 23);
+		dvChinh.add(weightAfter1);
 		
-		JRadioButton weightFirst = new JRadioButton("Cân trước");
-		weightFirst.addActionListener(new ActionListener() {
+		final JRadioButton weightFirst1 = new JRadioButton("Cân trước");
+		weightFirst1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				weightField.setEnabled(true);
+				weightField1.setEnabled(true);
 			}
 		});
-		weightFirst.setBounds(196, 137, 84, 23);
-		dvChinh.add(weightFirst);
+		weightFirst1.setBounds(230, 37, 84, 23);
+		dvChinh.add(weightFirst1);
+		
+		check1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				if (check1.isSelected()) {
+					weightFirst1.setEnabled(true);
+					weightAfter1.setEnabled(true);
+					weightField1.setEnabled(true);
+				}
+				else {
+					weightFirst1.setEnabled(false);
+					weightAfter1.setEnabled(false);
+					weightField1.setEnabled(false);
+				}
+			}
+		});
+		
+		weightFirst1.setSelected(true);
+		group1.add(weightFirst1);
+		group1.add(weightAfter1);
+		
+		////////////////////////////////////////////////////////////////////////GROUP BUTTON 2
+		final JCheckBox check2 = new JCheckBox(" Khối lượng giặt ướt");
+		check2.setFont(new Font("Times New Roman", Font.PLAIN, 22));
+		check2.setBounds(15, 155, 213, 35);
+		dvChinh.add(check2);
+		
+		final JRadioButton weightFirst2 = new JRadioButton("Cân trước");
+		weightFirst2.setBounds(230, 143, 84, 23);
+		weightFirst2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				weightField2.setEnabled(true);
+			}
+		});
+		dvChinh.add(weightFirst2);
+		
+		weightField2 = new JTextField();
+		weightField2.setColumns(10);
+		weightField2.setBounds(320, 143, 126, 20);
+		dvChinh.add(weightField2);
+		
+		final JRadioButton weightAfter2 = new JRadioButton("Cân sau");
+		weightAfter2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				weightField2.setEnabled(true);
+			}
+		});
+		weightAfter2.setBounds(230, 183, 84, 23);
+		dvChinh.add(weightAfter2);
+		
+		weightFirst2.setSelected(true);
+		group2.add(weightFirst2);
+		group2.add(weightAfter2);
+		
+		check2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				if (check2.isSelected()) {
+					weightFirst2.setEnabled(true);
+					weightAfter2.setEnabled(true);
+					weightField2.setEnabled(true);
+				}
+				else {
+					weightFirst2.setEnabled(false);
+					weightAfter2.setEnabled(false);
+					weightField2.setEnabled(false);
+				}
+			}
+		});
+		
+		if (check1.isSelected()) {
+			weightFirst1.setEnabled(true);
+			weightAfter1.setEnabled(true);
+			weightField1.setEnabled(true);
+		}
+		else if (check2.isSelected()) {
+			weightFirst2.setEnabled(true);
+			weightAfter2.setEnabled(true);
+			weightField2.setEnabled(true);
+		}
+		else if (check1.isSelected() && check2.isSelected()){
+			weightFirst1.setEnabled(true);
+			weightAfter1.setEnabled(true);
+			weightField1.setEnabled(true);
+			weightFirst2.setEnabled(true);
+			weightAfter2.setEnabled(true);
+			weightField2.setEnabled(true);
+		}
+		else {
+			weightFirst1.setEnabled(false);
+			weightAfter1.setEnabled(false);
+			weightField1.setEnabled(false);
+			weightFirst2.setEnabled(false);
+			weightAfter2.setEnabled(false);
+			weightField2.setEnabled(false);
+		}
 	}
 	
 	public void createDichVuPhu(JTabbedPane tabbedPane) {
 		dvPhu = new JPanel();
 		dvPhu.setLayout(null);
 		tabbedPane.addTab("DỊCH VỤ GIẶT PHỤ", null, dvPhu, null);
+		
+		khoAoKhoac = new JCheckBox(" Giặt khô áo khoác");
+		khoAoKhoac.setFont(new Font("Times New Roman", Font.PLAIN, 22));
+		khoAoKhoac.setBounds(24, 27, 193, 35);
+		dvPhu.add(khoAoKhoac);
+		
+		to1Field = new JTextField();
+		to1Field.setColumns(10);
+		to1Field.setBounds(309, 11, 126, 20);
+		dvPhu.add(to1Field);
+		
+		vua1Field = new JTextField();
+		vua1Field.setColumns(10);
+		vua1Field.setBounds(309, 37, 126, 20);
+		dvPhu.add(vua1Field);
+		
+		nho1Field = new JTextField();
+		nho1Field.setColumns(10);
+		nho1Field.setBounds(309, 63, 126, 20);
+		dvPhu.add(nho1Field);
+		
+		chan = new JCheckBox(" Giặt chăn");
+		chan.setFont(new Font("Times New Roman", Font.PLAIN, 22));
+		chan.setBounds(24, 144, 126, 35);
+		dvPhu.add(chan);
+		
+		to2Field = new JTextField();
+		to2Field.setColumns(10);
+		to2Field.setBounds(309, 128, 126, 20);
+		dvPhu.add(to2Field);
+		
+		vua2Field = new JTextField();
+		vua2Field.setColumns(10);
+		vua2Field.setBounds(309, 154, 126, 20);
+		dvPhu.add(vua2Field);
+		
+		nho2Field = new JTextField();
+		nho2Field.setColumns(10);
+		nho2Field.setBounds(309, 180, 126, 20);
+		dvPhu.add(nho2Field);
+		
+		to1 = new JLabel("To");
+		to1.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		to1.setBounds(239, 11, 30, 17);
+		dvPhu.add(to1);
+		
+		vua1 = new JLabel("Vừa");
+		vua1.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		vua1.setBounds(239, 38, 30, 17);
+		dvPhu.add(vua1);
+		
+		nho1 = new JLabel("Nhỏ");
+		nho1.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		nho1.setBounds(239, 66, 30, 17);
+		dvPhu.add(nho1);
+		
+		to2 = new JLabel("To");
+		to2.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		to2.setBounds(239, 128, 30, 17);
+		dvPhu.add(to2);
+		
+		vua2 = new JLabel("Vừa");
+		vua2.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		vua2.setBounds(239, 155, 30, 17);
+		dvPhu.add(vua2);
+		
+		nho2 = new JLabel("Nhỏ");
+		nho2.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		nho2.setBounds(239, 180, 30, 17);
+		dvPhu.add(nho2);
+		
+		khoAoKhoac.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				if (khoAoKhoac.isSelected()) {
+					to1.setEnabled(true); to1Field.setEnabled(true);
+					nho1.setEnabled(true); nho1Field.setEnabled(true);
+					vua1.setEnabled(true); vua1Field.setEnabled(true);
+				}
+				else {
+					to1.setEnabled(false); to1Field.setEnabled(false);
+					nho1.setEnabled(false); nho1Field.setEnabled(false);
+					vua1.setEnabled(false); vua1Field.setEnabled(false);
+				}
+			}
+		});
+		
+		chan.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				if (chan.isSelected()) {
+					to2.setEnabled(true); to2Field.setEnabled(true);
+					nho2.setEnabled(true); nho2Field.setEnabled(true);
+					vua2.setEnabled(true); vua2Field.setEnabled(true);
+				}
+				else {
+					to2.setEnabled(false); to2Field.setEnabled(false);
+					nho2.setEnabled(false); nho2Field.setEnabled(false);
+					vua2.setEnabled(false); vua2Field.setEnabled(false);
+				}
+			}
+		});
+		if (khoAoKhoac.isSelected()) {
+			to1.setEnabled(true); to1Field.setEnabled(true);
+			nho1.setEnabled(true); nho1Field.setEnabled(true);
+			vua1.setEnabled(true); vua1Field.setEnabled(true);
+		}
+		else if (chan.isSelected()) {
+			to2.setEnabled(true); to2Field.setEnabled(true);
+			nho2.setEnabled(true); nho2Field.setEnabled(true);
+			vua2.setEnabled(true); vua2Field.setEnabled(true);
+		}
+		else if (khoAoKhoac.isSelected() && chan.isSelected()){
+			to1.setEnabled(true); to1Field.setEnabled(true);
+			nho1.setEnabled(true); nho1Field.setEnabled(true);
+			vua1.setEnabled(true); vua1Field.setEnabled(true);
+			to2.setEnabled(true); to2Field.setEnabled(true);
+			nho2.setEnabled(true); nho2Field.setEnabled(true);
+			vua2.setEnabled(true); vua2Field.setEnabled(true);
+		}
+		else {
+			to1.setEnabled(false); to1Field.setEnabled(false);
+			nho1.setEnabled(false); nho1Field.setEnabled(false);
+			vua1.setEnabled(false); vua1Field.setEnabled(false);
+			to2.setEnabled(false); to2Field.setEnabled(false);
+			nho2.setEnabled(false); nho2Field.setEnabled(false);
+			vua2.setEnabled(false); vua2Field.setEnabled(false);
+		}
 	}
 	
 	public void createDichVuKhac(JTabbedPane tabbedPane) {
 		dvKhac = new JPanel();
 		dvKhac.setLayout(null);
 		tabbedPane.addTab("DỊCH VỤ GIẶT KHÁC", null, dvKhac, null);
+		
+		tenDV = new JLabel("Tên Dịch Vụ Khác");
+		tenDV.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		tenDV.setBounds(56, 37, 126, 20);
+		dvKhac.add(tenDV);
+		
+		textField = new JTextField();
+		textField.setColumns(10);
+		textField.setBounds(205, 24, 224, 54);
+		dvKhac.add(textField);
+		
+		soTien = new JLabel("Số tiền");
+		soTien.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		soTien.setBounds(56, 168, 126, 20);
+		dvKhac.add(soTien);
+		
+		textField_1 = new JTextField();
+		textField_1.setColumns(10);
+		textField_1.setBounds(205, 170, 126, 20);
+		dvKhac.add(textField_1);
 	}
 	
 	public void createThanhToan(JTabbedPane tabbedPane) {
 		thanhToan = new JPanel();
 		thanhToan.setLayout(null);
 		tabbedPane.addTab("THANH TOÁN TIỀN", null, thanhToan, null);
+		
+		traTruoc = new JRadioButton("Trả trước");
+		traTruoc.setSelected(true);
+		traTruoc.setBounds(335, 35, 84, 23);
+		thanhToan.add(traTruoc);
+		
+		traSau = new JRadioButton("Trả sau");
+		traSau.setBounds(335, 99, 84, 23);
+		thanhToan.add(traSau);
+		
+		totalAmount = new JLabel("TỔNG SỐ TIỀN THANH TOÁN");
+		totalAmount.setFont(new Font("Tahoma", Font.BOLD, 16));
+		totalAmount.setBounds(27, 23, 260, 43);
+		thanhToan.add(totalAmount);
+		
+		numberTotal = new JLabel("");
+		numberTotal.setFont(new Font("Tahoma", Font.BOLD, 16));
+		numberTotal.setBounds(27, 87, 260, 43);
+		numberTotal.setText(numberTotalAmount + " đồng");
+		thanhToan.add(numberTotal);
+		
+		JButton confirmButton = new JButton("Hoàn thành");
+		confirmButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
+		confirmButton.setFont(new Font("Tahoma", Font.BOLD, 14));
+		confirmButton.setBounds(310, 168, 159, 43);
+		thanhToan.add(confirmButton);
+		
+		printButton = new JButton("Xem hóa đơn");
+		printButton.setFont(new Font("Tahoma", Font.BOLD, 14));
+		printButton.setBounds(128, 168, 159, 43);
+		thanhToan.add(printButton);
 	}
 	
 	public TakeFromCustomers() {
 		try {
 			UIManager.setLookAndFeel( UIManager.getSystemLookAndFeelClassName() );
 		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (InstantiationException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IllegalAccessException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (UnsupportedLookAndFeelException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		createTextpaneAndTextfield();
---
+++ laundry.app/src/main/java/org/lma/app/printForm.java
@@ -0,0 +1,179 @@
+package org.lma.app;
+
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.ActionListener;
+import java.awt.print.PageFormat;
+import java.awt.print.Printable;
+import java.awt.print.PrinterException;
+import java.awt.print.PrinterJob;
+import java.awt.event.ActionEvent;
+
+@SuppressWarnings("serial")
+public class printForm extends JFrame {
+
+	private JPanel contentPanel;
+	private JFrame frame;
+	public ImageIcon img = new ImageIcon("./icon app.png");
+	private JLabel lblMSHa;
+	private JLabel lblThiGianLp;
+	private JLabel lblTnKhchHng;
+	private JLabel lblDchVGit;
+	private JLabel lblThanhTon;
+	private JLabel lblDchVGit_1;
+	private JLabel lblDchVGit_2;
+	private JLabel label;
+	private JLabel label_1;
+	private JLabel label_2;
+	private JLabel label_3;
+	private JLabel label_4;
+	private JLabel label_5;
+	private JLabel label_6;
+	private JButton btnInHan;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		new printForm();
+	}
+	
+	public void createTextpaneAndTextfield() {
+		contentPanel = new JPanel();
+		
+		final JPanel printPanel = new JPanel();
+		printPanel.setBounds(10, 11, 564, 601);
+		printPanel.setLayout(null);
+		contentPanel.add(printPanel);
+		
+		lblMSHa = new JLabel("Mã số hóa đơn");
+		lblMSHa.setBounds(31, 11, 85, 31);
+		printPanel.add(lblMSHa);
+		
+		lblThiGianLp = new JLabel("Thời gian lập hóa đơn");
+		lblThiGianLp.setBounds(31, 39, 132, 31);
+		printPanel.add(lblThiGianLp);
+		
+		lblTnKhchHng = new JLabel("Tên Khách Hàng");
+		lblTnKhchHng.setBounds(31, 70, 115, 31);
+		printPanel.add(lblTnKhchHng);
+		
+		lblDchVGit = new JLabel("Dịch vụ giặt chính");
+		lblDchVGit.setBounds(31, 112, 103, 31);
+		printPanel.add(lblDchVGit);
+		
+		lblThanhTon = new JLabel("Thanh toán");
+		lblThanhTon.setBounds(31, 238, 103, 31);
+		printPanel.add(lblThanhTon);
+		
+		lblDchVGit_1 = new JLabel("Dịch vụ giặt phụ");
+		lblDchVGit_1.setBounds(31, 154, 103, 31);
+		printPanel.add(lblDchVGit_1);
+		
+		lblDchVGit_2 = new JLabel("Dịch vụ giặt khác");
+		lblDchVGit_2.setBounds(31, 196, 103, 31);
+		printPanel.add(lblDchVGit_2);
+		
+		label = new JLabel("Thanh toán");
+		label.setBounds(31, 548, 103, 31);
+		printPanel.add(label);
+		
+		label_1 = new JLabel("Dịch vụ giặt khác");
+		label_1.setBounds(31, 506, 103, 31);
+		printPanel.add(label_1);
+		
+		label_2 = new JLabel("Dịch vụ giặt phụ");
+		label_2.setBounds(31, 460, 103, 31);
+		printPanel.add(label_2);
+		
+		label_3 = new JLabel("Dịch vụ giặt chính");
+		label_3.setBounds(31, 418, 103, 31);
+		printPanel.add(label_3);
+		
+		label_4 = new JLabel("Tên Khách Hàng");
+		label_4.setBounds(31, 380, 115, 31);
+		printPanel.add(label_4);
+		
+		label_5 = new JLabel("Thời gian lập hóa đơn");
+		label_5.setBounds(31, 351, 132, 31);
+		printPanel.add(label_5);
+		
+		label_6 = new JLabel("Mã số hóa đơn");
+		label_6.setBounds(31, 321, 85, 31);
+		printPanel.add(label_6);
+		
+		btnInHan = new JButton("In hóa đơn");
+		btnInHan.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				final PrinterJob job = PrinterJob.getPrinterJob();
+				job.setJobName("IN_HOA_DON");
+				job.setPrintable(new Printable() {
+
+					public int print(Graphics pg, PageFormat pf, int pageNum) throws PrinterException {
+						if (pageNum > 0) {
+							return Printable.NO_SUCH_PAGE;
+						}
+						// get the bounds of the component
+					    Dimension dim = printPanel.getSize();
+					    double cHeight = dim.getHeight();
+					    double cWidth = dim.getWidth();
+
+					    // get the bounds of the printable area
+					    double pHeight = pf.getImageableHeight();
+					    double pWidth = pf.getImageableWidth();
+
+					    double pXStart = pf.getImageableX();
+					    double pYStart = pf.getImageableY();
+
+					    double xRatio = pWidth / cWidth;
+					    double yRatio = pHeight / cHeight;
+						
+						Graphics2D g2 = (Graphics2D)pg;
+						g2.translate(pXStart, pYStart);
+					    g2.scale(xRatio, yRatio);
+						printPanel.paint(g2);
+						
+						return Printable.PAGE_EXISTS;
+					}
+				});
+				boolean ok = job.printDialog();
+				if (ok) {
+					try {
+						job.print();
+					}
+					catch (PrinterException ex) {
+						ex.printStackTrace();
+					}
+				}
+			}
+		});
+		btnInHan.setFont(new Font("Tahoma", Font.BOLD, 16));
+		btnInHan.setBounds(435, 617, 139, 33);
+		contentPanel.add(btnInHan);
+	}
+
+	public void createFrame() {
+		this.frame = new JFrame();
+		this.frame.setTitle("Quick Laundry Management");
+		this.frame.setIconImage(img.getImage());
+		this.contentPanel.setLayout(null);
+		this.frame.setContentPane(this.contentPanel);
+		this.frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		this.frame.getContentPane().setLayout(null);
+		
+		this.frame.setBounds(20, 20, 600, 700);
+		this.frame.setVisible(true);
+		this.frame.addWindowListener(new java.awt.event.WindowAdapter() {
+			@Override
+			public void windowClosing(java.awt.event.WindowEvent windowEvent) {
+				frame.dispose();
+			}
+		});
+		this.frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+	}
+	
+	public printForm() {
+		createTextpaneAndTextfield();
+		createFrame();
+	}
+}
